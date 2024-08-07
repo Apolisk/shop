2cb007f24098446326310d22d32e6e3facd633d7
Apolisk <a.bogdanovitch8@gmail.com>
2024-08-07 16:46:20 +0300

    07-08-2024

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@
 
 let tg = window.Telegram.WebApp;
 tg.expand();
-let user = document.getElementById("user ");
+let user = document.getElementById("user");
 let p = document.createElement("p");
 
 p.innerText=`${tg.initDataUnsafe.first_name}`;
